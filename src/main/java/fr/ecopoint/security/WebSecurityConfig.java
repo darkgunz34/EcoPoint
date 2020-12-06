3636a47845d4d3f2fce6a94f4ee0d8eec66fa40b
darkunz34 <stephan.parichon.pro@gmail.com>
2020-12-06 21:56:33 +0100

    Création de l'application spring avec par défault de la configuration. Mise en place d'une configuration minimale pour le projet. Mise en place de spring security. Mise en place d'un test unitaire pour le port 9090.

---
+++ src/main/java/fr/ecopoint/security/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package fr.ecopoint.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    public WebSecurityConfig(AuthenticationSuccessHandler authenticationSuccessHandler) {
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/static/css/**").permitAll()
+                .antMatchers("/static/js/**").permitAll()
+                .antMatchers("/utilisateur/**").hasRole("UTILISATEUR")
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .successHandler(authenticationSuccessHandler)
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and().csrf().disable();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
